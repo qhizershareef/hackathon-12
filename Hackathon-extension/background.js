d3e9f29a040d108d38a445309f32150b28bb9f6c
ShareefAhmed <qhizer.syed@ais.com>
2022-11-09 01:16:34 +0530

    Modal and DB

--- Hackathon-extension/background.js
+++ Hackathon-extension/background.js
@@ -1,30 +1,121 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
+try {
+  //import firebase scripts from firebase folder
+    self.importScripts('firebase/firebase-firebase.js');
+  // // Initialize Firebase
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBECZ0MkXb6MAQVgx6XP_jH542GEDpid_M",
+      authDomain: "social-network-js.firebaseapp.com",
+      databaseURL: "https://social-network-js.firebaseio.com",
+      projectId: "social-network-js",
+      storageBucket: "social-network-js.appspot.com",
+      messagingSenderId: "640569497422",
+      appId: "1:640569497422:web:ae448c5f633dae0e63f01f",
+      measurementId: "G-K7CJ3C2D49"
+    };
+    //   // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    var db = firebase.firestore();
+    console.log(db)
+
+  
+
+  chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url) {
-    //   const queryParameters = tab.url.split("?")[1];
-    //this piece of code is very important it determines if the url path has changed 
-    //   const urlParameters = new URLSearchParams(queryParameters);
+      // const queryParameters = tab.url.split("?")[1];
+      // this piece of code is very important it determines if the url path has changed 
+      // const urlParameters = new URLSearchParams(queryParameters);
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
         url: tab.url,
       });
     }
   });
-console.log('inside background')
-let contextMenuItem = {
+  console.log('inside background')
+  let contextMenuItem = {
     id: 'add',
     title: 'Save this code?',
     contexts: ['selection']
-}
-chrome.contextMenus.create(contextMenuItem);
+  }
+  // chrome.contextMenus.create(contextMenuItem);
 
-chrome.contextMenus.onClicked.addListener((clickData)=>{
-    if(clickData.menuItemId === 'add' && clickData.selectionText){
-        console.log(clickData.selectionText)
-        //send this to the content script
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
-            chrome.tabs.sendMessage(tabs[0].id, {message: 'add', selection: clickData.selectionText})
+  //fix chrome runtime error cannot create duplicate id add.
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create(contextMenuItem);
+  })
+
+  //when clicked on context menu item, send message to content script to show modal
+  chrome.contextMenus.onClicked.addListener((data) => {
+    if (data.menuItemId === 'add' && data.selectionText) {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          message: 'displayModal',
+          selection: data.selectionText
         })
+      })
     }
-}
-)
+  })
 
+  //listen for message from content script
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === 'NEW') {
+      console.log('new ' + location.href);
+    }
+    if (request.message === 'save') {
+      console.log('adding message to firebase')
+      console.log(request)
+      //store this as object in firestore db
+      db.collection("code").add({
+        codeName: request.codeName,
+        code: request.code,
+        name: request.userName,
+        isPrivate: request.isPrivate,
+      }).then((docRef) => {
+        console.log('code saved', docRef.id)
+      }).catch((error) => {
+        console.log('error saving code', error)
+      })
+    }
+    })
+
+
+
+
+  // chrome.contextMenus.onClicked.addListener((data) => {
+  //   console.log('inside context menu')
+  //   if (data.menuItemId === 'add' && data.selectionText) {
+  //     console.log('inside add data');
+  //     console.log('data', data.selectionText);
+  //     //send data to content script
+  //     // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     //   chrome.tabs.sendMessage(tabs[0].id, {
+  //     //     message: 'add',
+  //     //     selection: data.selectionText
+  //     //   })
+  //     // })
+  //     //use firebase latest version
+  //       db.collection('code').add({
+  //           code: data.selectionText,
+  //       }).then((docRef)=>{
+  //           console.log('code saved', docRef.id)
+  //       }).catch((error)=>{
+  //           console.log('error saving code', error)
+  //       })
+  //     }
+  // })
+
+  // chrome.contextMenus.onClicked.addListener((clickData)=>{
+  //     if(clickData.menuItemId === 'add' && clickData.selectionText){
+  //         console.log(clickData.selectionText)
+  //         //send this to the content script
+  //         chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+  //             chrome.tabs.sendMessage(tabs[0].id, {message: 'add', selection: clickData.selectionText})
+  //         })
+  //     }
+  // })
+
+
+} catch (error) {
+  console.log(error);
+}
--- Hackathon-extension/contentScript.js
+++ Hackathon-extension/contentScript.js
@@ -1,5 +1,5 @@
 (()=>{
-    console.log('contentscript');
+    
 
     function createDropDown(){
         if(document.getElementById('dropdown')){
@@ -24,6 +24,52 @@
         const divPath = document.querySelector('header >div>div')
         divPath.appendChild(div);
     }
+
+    function displayModal(data){
+        if(document.getElementById('modalSave')){
+            return;
+        }
+        console.log('data', data);
+        let div = document.createElement('div');
+        div.id = 'modalSave';
+        div.style = 'display: flex;justify-content:center;align-items:center;position:fixed;top:0;left:0;right:0;bottom:0;background-color:rgba(0,0,0,0.5);z-index:999';
+        div.innerHTML=`<div id="modal" style="position: absolute; top:100px; width: 300px; height: 400px; background-color: white; z-index: 100; border: 1px solid black; padding: 10px; box-sizing: border-box; overflow: auto; display: flex; flex-direction: column; justify-content: space-between;">
+            <input type="text" id="username" placeholder="Enter name" style="margin-bottom: 10px;">
+            <input type="text" id="Queryname" placeholder="Query Name" style="margin-bottom: 10px;">
+            <textarea id="code" style="margin-bottom: 10px; height: 200px; width: 100%; box-sizing: border-box; border: 1px solid black; padding: 5px; font-family: monospace; font-size: 14px;">
+                ${data}
+            </textarea>
+
+            <input type="checkbox" id="isPrivate" style="margin-bottom: 10px;">
+            <label for="isPrivate">Private</label>
+            <button id="save" style="margin-bottom: 10px;">Save</button>
+            <button id="cancel" style="margin-bottom: 10px;">Cancel</button>
+        </div>`
+        //     <button style="cursor: pointer; background-color: black; color: white; border: none; padding: 5px 10px; border-radius: 5px;">Save</button>
+        // </div>`;
+        div.querySelector('button').addEventListener('click',()=>{
+            let codeName = div.querySelector('#Queryname').value;
+            let userName = div.querySelector('#username').value;
+            // let code = div.querySelector('.code').value;
+
+            if(code){
+                chrome.runtime.sendMessage({
+                    message: 'save',
+                    codeName: codeName,
+                    userName: userName,
+                    code: data,
+                    isPrivate: div.querySelector('#isPrivate').checked,
+                })
+                div.remove();
+            }
+        })
+        div.querySelector('#cancel').addEventListener('click',()=>{
+            div.remove();
+        })
+        document.body.appendChild(div);
+
+    }
+
 
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         if(request.type === 'NEW'){
@@ -58,6 +104,11 @@
                 })
             })            
         }
+        if(request.message==='addToFirebase'){
+            console.log('adding to firebase')
+            console.log(request.selection)
+            //store this as object in firestore
+        }
         if(request.message==='getData'){
             chrome.storage.local.get(['code'], (result)=>{
                 console.log(result.code)
@@ -82,6 +133,9 @@
             let dropDown = document.querySelector('#dropdown');
             //delete drop down
             dropDown.remove();
+        }
+        if( request.message==='displayModal'){
+            displayModal(request.selection);
         }
 
         // if (request.message === 'background') {
